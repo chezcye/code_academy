bbe0f91257a82ea36f2b148ab6d5b265f03a792b
T'Cheznavia Cherry <chezcye@gmail.com>
2023-03-21 14:22:04 -0400

    Update app.js

--- full-stack-engineer-career-path/interactive-websites/javascript-syntax/introduction/app.js
+++ full-stack-engineer-career-path/interactive-websites/javascript-syntax/introduction/app.js
@@ -1,1 +1,13 @@
+// This is a constant variable to store the forecast for today weather in Kelvin.
+const kelvin = 0;
+// A constant variable to store kelvin variable in celsius. 
+const celsius = kelvin - 273;
+// Utilizing the fahrennheit formula to convert celsius into fahrenheit.  
+let fahrenheit = celsius * (9/5) + 32;
+// Using Math.floor to round the fahrenheit variable down to the nearest temperature.
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
