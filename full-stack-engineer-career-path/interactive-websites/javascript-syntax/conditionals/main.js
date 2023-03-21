e0c958ad9bb00a07f8e3b1a30c47dfe920f5343b
T'Cheznavia Cherry <chezcye@gmail.com>
2023-03-21 14:38:19 -0400

    Update main.js

--- full-stack-engineer-career-path/interactive-websites/javascript-syntax/conditionals/main.js
+++ full-stack-engineer-career-path/interactive-websites/javascript-syntax/conditionals/main.js
@@ -1,1 +1,16 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly =  false;
+let runnerAge = 18;
+ if (runnerAge > 18 && registeredEarly === true ) {
+   console.log(raceNumber += 100);
+ } else if (runnerAge > 18 && registeredEarly != true){
+   console.log(`Your race will start at 11:00 am. Your race number is: ${raceNumber}`);
+ } else if (runnerAge < 18){
+   console.log(`Your race will start at 12:30pm. Your race number ${raceNumber}`);
+ } else {
+   console.log('You need to go see the registration desk.');
+ }
 
+ if (runnerAge > 18 && registeredEarly === true){
+   console.log(`Your race will start at 9:30am. Your race number is: ${raceNumber}`);
+ }
