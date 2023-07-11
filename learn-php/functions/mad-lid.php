f6f4dccbeab2c4be6aed48813ba88e9b3b609402
T'Cheznavia Cherry <chezcye@gmail.com>
2023-07-11 19:14:19 -0400

    Update mad-lid.php

--- learn-php/functions/mad-lid.php
+++ learn-php/functions/mad-lid.php
@@ -1,8 +1,10 @@
 <?php
 // Create a function generateStory with three input parameters: $singular_noun, $verb, $color
 function generateStory($singular_noun, $verb, $color, $distance_unit) {
-  // Create a variable $story and assign it the last stanza of Robert Frost’s “Stopping by Woods on a Snowy Evening”. Use newlines (\n) to ensure the lines break in the right location. 
-  // Within the function, change the $story string so that it parses the $singularNoun variable where the word wood currently is, $verb where the word sleep appears (both locations), and $color where dark appears.
+  // Create a variable $story and assign it the last stanza of Robert Frost’s “Stopping by Woods on a Snowy Evening”. 
+  // Use newlines (\n) to ensure the lines break in the right location. 
+  // Within the function, change the $story string so that it parses the $singularNoun variable where the word wood currently is,
+  // $verb where the word sleep appears (both locations), and $color where dark appears.
   // Add one more parameter to the function and allow the word miles to be replaced with a $distance_unit when the function is called (both places).
   $story = "\nThe $singular_noun are lovely, $color and deep. \nBut I have promises to keep \nAnd $distance_unit to go before I $verb, 
 \nAnd $distance_unit to go before I $verb.\n";
