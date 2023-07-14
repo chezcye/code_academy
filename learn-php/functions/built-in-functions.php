c5ae072d72368563f3d1a5334040987e0fd43d0e
T'Cheznavia Cherry <chezcye@gmail.com>
2023-07-14 13:12:46 -0400

    Create built-in-functions.php

---
+++ learn-php/functions/built-in-functions.php
@@ -0,0 +1,36 @@
+<?php
+$initial = '555';
+
+$a = octdec($initial);
+echo $a;
+echo "\n";
+$b = deg2rad($a);
+echo $b;
+echo "\n";
+$c = cos($b);
+echo $c;
+echo "\n";
+$d = round($c, 3);
+echo $d;
+$e = log($d);
+echo "\n";
+echo $e;
+$f = abs($e);
+echo "\n";
+echo $f;
+$g = acos($f);
+echo "\n";
+echo $g;
+$h = rad2deg($g);
+echo "\n";
+echo $h;
+$i = floor($h);
+echo "\n";
+echo $i;
+$j = $i - 47; 
+echo "\n";
+echo $j;
+
+
+
+
