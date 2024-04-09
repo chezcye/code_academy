a792e8bd4635fb3f06947d51302f75cf3c6d0a03
T'Cheznavia Cherry <chezcye@gmail.com>
2024-04-09 16:16:04 -0400

    Update initials.swift

--- iOS Developer/block letters/initials.swift
+++ iOS Developer/block letters/initials.swift
@@ -1,4 +1,8 @@
+// First, write a comment with a fun fact about yourself!
+
+
 print("Fun Fact: I love to cooking")
+// Output your first and second initial as a block letter.
 print("TTTTT   CCC ")
 print("  T   C    C ")
 print("  T   C")
